e991e75d3ca1c620d22f4ce2280453b193bcd255
Gordon <gordon.zhu25@ycdsbk>
2023-10-24 16:53:44 -0400

    Added Christmas Promotions

--- TeamCode/src/main/java/team_19447/teleOp_MotorTesting.java
+++ TeamCode/src/main/java/team_19447/teleOp_MotorTesting.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-//This file is not used
+//This file is not used for competition
 @TeleOp
 public class teleOp_MotorTesting extends LinearOpMode {
 
