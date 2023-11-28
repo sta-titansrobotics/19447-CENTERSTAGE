722df9e394148a85650ae9a37058d56a6ab2b43c
ellas <ella.yap26@ycdsbk12.ca>
2023-11-28 15:39:45 -0500

    modified teleop code

--- TeamCode/src/main/java/team_19447/teleOp_Drivetrain47.java
+++ TeamCode/src/main/java/team_19447/teleOp_Drivetrain47.java
@@ -18,11 +18,6 @@ public class teleOp_Drivetrain47 extends LinearOpMode {
     int button2X =0;
     int button2B =0;
     int button2Y =0;
-
-    boolean dropping = false;
-    //change -6 to how long it takes for the servo to change -1
-    //- number to prevent the later if statement from being executed at the start
-    int prevtime = -6;
     @Override
     public void runOpMode() {
 
@@ -34,21 +29,16 @@ public class teleOp_Drivetrain47 extends LinearOpMode {
         DcMotor motorBR = hardwareMap.get(DcMotor.class, "motorBackRight");
 
         /*
-        DcMotor Intake = hardwareMap.get(DcMotor.class, "Intake"); --> Done
-        DcMotor Sliders = hardwareMap.get(DcMotor.class, "Sliders"); --> Done
-        DcMotor Climbing1 = hardwareMap.get(DcMotor.class, "Climbing1"); for the robot to hang --> Done
-        DcMotor Climbing2 = hardwareMap.get(DcMotor.class, "Climbing2"); for the robot to hang --> Done
-
-        Servo DropperTop =  hardwareMap.get(Servo.class, "DropperTop"); --> Done   //Servo Port 0
-        Servo DropperBottom =  hardwareMap.get(Servo.class, "DropperBottom"); --> Done //Servo Port 1
-        Servo Wrist =  hardwareMap.get(Servo.class, "Wrist"); --> the thing that rotates the dropper //Servo Port 2
-        Servo AirplaneLauncher = hardwareMap.get(Servo.class, "AirplaneLauncher"); --> Still have to work on //Servo Port 3
-
-        Climbing1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        Climbing2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        DcMotor Intake = hardwareMap.get(DcMotor.class, "Intake");
+        DcMotor Sliders = hardwareMap.get(DcMotor.class, "Sliders");
+        DcMotor Climbing = hardwareMap.get(DcMotor.class, "Climbing");
+        Servo ClawDrop1 =  hardwareMap.get(Servo.class, "ClawDrop1");
+        Servo ClawDrop2 =  hardwareMap.get(Servo.class, "ClawDrop2");
+        Servo Wrist =  hardwareMap.get(Servo.class, "Wrist");
+        Servo AirplaneLauncher = hardwareMap.get(Servo.class, "Airplane Launcher");
         */
 
-        //toggle template, note: its just template, delete after
+        //toggle template
         if (gamepad1.a)
             buttonA +=1;
         if(buttonX%2==1){
@@ -57,11 +47,7 @@ public class teleOp_Drivetrain47 extends LinearOpMode {
             //Intake.setpower(0);
         }
 
-        //wrist
-        //the strange calculations are because we need to convert (-1 - 1) into (0 - 1)
-        //Wrist.setPosition((gamepad2.left_stick_y*0.5)+0.5);
-
-        //Intake
+        //intake
         /*
         if (gamepad1.a)
             buttonA +=1;
@@ -72,40 +58,38 @@ public class teleOp_Drivetrain47 extends LinearOpMode {
         }
         */
 
-        //Climbing: if gamepad2 X is pressed, the climbing motors will go at full power and push the linear actuators up to a certain positiion
-        // If the button X is pressed again, then the actuators will descend to [some certain point in terms of encoder reading] at full power and hang the robot
-        if (gamepad2.x)
-            button2X +=1;
-
-        //Note: the encoder values are placeholders
-        if(buttonX%2==1){
-            //Climbing1.setTargetPosition(5000);
-            //Climbing2.setTargetPosition(5000);
-        }else {
-            //Climbing1.setTargetPosition(2000);
-            //Climbing2.setTargetPosition(2000);
-        }
-
-        //dropper
-        //drops the bottom slot then waits until button is not pressed
-        //when button is not pressed load the top slot into the bottom slot
-
+        //Climbing
+        //0.1 because stick drift
         /*
-        if (gamepad2.b){
-            DropperBottom.setPosition(0.5);
-            dropping = true;
-        }
-        if (!gamepad2.b && dropping) {
-            DropperBottom.setPosition(0);
-            prevtime = getRuntime();
-            DropperTop.setPosition(0.5);
-            dropping = false;
-        }
-        //change 5 to however long it takes for the servo to move into place
-        if (getRuntime() - prevtime == 2000){
-            DropperTop.setPosition(0);
-        }
-        */
+        if (gamepad2.right_stick_y > 0.1){
+            Climbing.setpower(gamepad2.right_stick_y);
+        } else if (gamepad2.right_stick_y < -0.1){
+            Climbing.setpower(-gamepad2.right_stick_y);
+
+         */
+
+        //ClawDrop1
+        if (gamepad2.a)
+            button2A +=1;
+        if(button2A%2==1){
+            //ClawDrop1.setPower(1);
+        }else{
+            //ClawDrop1.setpower(0);
+
+        //ClawDrop2
+        if (gamepad2.a)
+            button2X +=1;
+        if(button2X%2==1){
+            //ClawDrop2.setpower(1);
+        }else{
+            //ClawDrop2.setpower(0);
+
+            //AirplaneLauncher
+            if (gamepad2.x)
+                button2X +=1;
+            if(button2X%2==1)
+        }else{
+            //AirplaneLauncher.setpower(0);
 
         //Reverse right side motors
         motorFL.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -134,7 +118,8 @@ public class teleOp_Drivetrain47 extends LinearOpMode {
             if(Math.abs(x)<0.1)
                 x=0;
 
-            //Basic movement
+            //Basic movements
+            /*
             if(x>0&&y>0){
                 //Moving forward-right (diagonally)
                 motorFL.setPower(1);
@@ -181,7 +166,7 @@ public class teleOp_Drivetrain47 extends LinearOpMode {
                 motorBL.setPower(0);
                 motorFR.setPower(0);
                 motorBR.setPower(0);
-            }
+            }*/
 
             double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
             double frontLeftPower = (y + x + rx) / denominator;
@@ -201,4 +186,4 @@ public class teleOp_Drivetrain47 extends LinearOpMode {
             telemetry.update();
         }
     }
-}
+}}}
