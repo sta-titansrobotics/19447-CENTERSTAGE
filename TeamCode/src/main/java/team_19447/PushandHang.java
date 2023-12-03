b0e9c27390d3c8b557bb2d8aa45e2a6f038e3ac6
Gordon <gordon.zhu25@ycdsbk>
2023-12-03 13:14:00 -0500

    heheheha

--- TeamCode/src/main/java/team_19447/PushandHang.java
+++ TeamCode/src/main/java/team_19447/PushandHang.java
@@ -10,13 +10,7 @@ import com.qualcomm.robotcore.util.Range;
 
 
 @TeleOp
-public class AnotherDriveTrain47 extends LinearOpMode {
-
-    int buttonA=0;
-    int isClimbing=0;
-    double wristpower = 0;
-    int isSliding = 0 ;
-    int buttonX=0;
+public class PushandHang extends LinearOpMode {
 
     ElapsedTime timer = new ElapsedTime();
 
@@ -42,14 +36,14 @@ public class AnotherDriveTrain47 extends LinearOpMode {
         Servo DropperBottom = hardwareMap.get(Servo.class, "DropperBottom"); // control hub servo port 2
         Servo Wrist = hardwareMap.get(Servo.class, "Wrist"); // control hub servo port 0
 
-        //Climbing1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        /*//Climbing1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         Climbing1.setDirection(DcMotorSimple.Direction.REVERSE);
 
         Climbing1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         Climbing2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         Sliders.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         //Climbing2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        Climbing2.setDirection(DcMotorSimple.Direction.REVERSE);
+        Climbing2.setDirection(DcMotorSimple.Direction.REVERSE);*/
 
         //Reverse right side motors
         motorFL.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -67,80 +61,25 @@ public class AnotherDriveTrain47 extends LinearOpMode {
                 Intake.setPower(0);
 
             if (gamepad2.right_trigger > 0) {
-                Intake.setPower(-gamepad2.right_trigger);
+                Intake.setPower(-gamepad2.right_trigger/2);
             }
 
-
-            //Slider
-            if (Sliders.getCurrentPosition() > 7500)
-                isSliding = 0;
-            if (gamepad1.right_bumper) {
-                isSliding = 1;
-            }
-
-            if (isSliding==1 && Sliders.getCurrentPosition() < 7500) {
-                Sliders.setPower(1);
-            } else if (gamepad1.right_trigger > 0 && Sliders.getCurrentPosition() > 100) {
-                Sliders.setPower(-gamepad1.right_trigger);
-            } else {
-                Sliders.setPower(0);
-            }
-
-
-            //Climbing:  mapped to right joystick power
 
             Climbing1.setPower(gamepad2.right_stick_y);
             Climbing2.setPower(gamepad2.right_stick_y);
 
-            //dropper
-            //drops the bottom slot then waits until button is not pressed
-            //when button is not pressed load the top slot into the bottom slot
-            if (gamepad2.x && timer.seconds() > 0.5) {
-                buttonX += 1;
-                timer.reset();
-            }
-
-            //Note: top and bottom servo have wierd ranges as the servo programmer
-            // was bugging so range had to be adjusted to whatever range I can set it to
-            // update: ignore?
-            if (buttonX % 4 == 1)
-                DropperBottom.setPosition(0);
-            else if (buttonX % 4 == 2) {
-                DropperTop.setPosition(1);
-
-            } else if (buttonX % 4 == 3) {
-                DropperTop.setPosition(0);
-            } else {
-                DropperBottom.setPosition(1);
-            }
-
-            //wrist
-            //the strange calculations are because we need to convert (-1 - 1) into (0 - 1)
-            if (gamepad1.left_bumper) {
-                wristpower += 0.02;
-                sleep(20);
-            }//fdsa
-
-            if (gamepad1.right_trigger < 0.5){
-                wristpower -= 0.02;
-                sleep(20);
-            }
-
-            wristpower = Range.clip(wristpower, 0, 1);
-
-            Wrist.setPosition(wristpower);
 
             //------------------DRIVE TRAIN---------------------------------
             //Driving
             double leftPower = -gamepad1.left_stick_y;
             double rightPower = -gamepad1.right_stick_y; // heheheha
 
-            if (gamepad1.right_stick_x > 0.7) {
+            if (gamepad1.right_stick_x > 0.5) {
                 motorFL.setPower(gamepad1.right_stick_x);
                 motorBL.setPower(-gamepad1.right_stick_x);
                 motorFR.setPower(-gamepad1.right_stick_x);
                 motorBR.setPower(gamepad1.right_stick_x);
-            } else if (gamepad1.left_stick_x < -0.7) {
+            } else if (gamepad1.left_stick_x < -0.5) {
                 motorFL.setPower(gamepad1.left_stick_x);
                 motorBL.setPower(-gamepad1.left_stick_x);
                 motorFR.setPower(-gamepad1.left_stick_x);
